376af92dedede2d1b6ec8a5ffd9f728aff2c148b
Alexme116 <alexmoncada16@hotmail.com>
2024-04-23 20:28:45 -0600

    Operadores y parametros de funciones implementados

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ def lineRead(line, contenido_html):
         contenido_html = comentarioEncontrado(line, contenido_html)
     elif re.search(r'\bdef\b', line):
         contenido_html = funcionEncontrada(line, contenido_html)
-    elif re.search(r'\+|\-', line):
-        print("ENCONTRADO")
+    elif re.search(r'\+|\-|\*|\^|\/|\%|\&\&|\|{2}|\!', line):
         contenido_html = operadorEncontrado(line, contenido_html)
     else:
         contenido_html += f"<p>{line}</p>"
@@ -18,13 +17,32 @@ def comentarioEncontrado(line, contenido_html):
     return contenido_html
 
 def funcionEncontrada(line, contenido_html):
-    resultado = re.sub(r'\bdef\b', '<span class="function">def</span>', line)
+    prueba = re.findall(r'\((.*?)\)', line)
+    resultado = re.sub(rf'{prueba[0]}', f'<span class="param">{prueba[0]}</span>', line)
+    resultado = re.sub(r'\bdef\b', '<span class="function">def</span>', resultado)
     contenido_html += f"<p>{resultado}</p>"
     return contenido_html
 
 def operadorEncontrado(line, contenido_html):
-    operador = re.search(r'\+', line)
-    resultado = re.sub(r'\+', f'<span class="operator">{operador.group(0)}</span>', line)
+    resultado = line
+    if re.search(r'\/', line):
+        resultado = re.sub(r'\/', f'<span class="operator">/</span>', resultado)
+    if re.search(r'\+', line):
+        resultado = re.sub(r'\+', f'<span class="operator">+</span>', resultado)
+    if re.search(r'\-', line):
+        resultado = re.sub(r'\-', f'<span class="operator">-</span>', resultado)
+    if re.search(r'\*', line):
+        resultado = re.sub(r'\*', f'<span class="operator">*</span>', resultado)
+    if re.search(r'\^', line):
+        resultado = re.sub(r'\^', f'<span class="operator">^</span>', resultado)
+    if re.search(r'\%', line):
+        resultado = re.sub(r'\%', f'<span class="operator">%</span>', resultado)
+    if re.search(r'\&\&', line):
+        resultado = re.sub(r'\&\&', f'<span class="operator">&&</span>', resultado)
+    if re.search(r'\|{2}', line):
+        resultado = re.sub(r'\|{2}', f'<span class="operator">||</span>', resultado)
+    if re.search(r'\!', line):
+        resultado = re.sub(r'\!', f'<span class="operator">!</span>', resultado)
     contenido_html += f"<p>{resultado}</p>"
     return contenido_html
 
@@ -41,6 +59,9 @@ contenido_html = """
             font-family: Arial, sans-serif;
             background-color: #f0f0f0;
             color: #333;
+        }
+        .param {
+            color: #1129ff;
         }
         .comment {
             color: #008206;
